9f6d251feca11f5b88b3c1ffc9a9b64c34c6f222
marialovgren <marialovgren86@hotmail.com>
2022-03-04 18:22:55 +0100

    changed name on album in validation to album_validation

--- routes/album_route.js
+++ routes/album_route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const albumController = require('../controllers/album_controller');
-const albumValidationRules = require('../validation/album');
+const albumValidationRules = require('../validation/album_validation');
 
 /* Hämta alla album */
 // GET /albums
@@ -19,3 +19,12 @@ router.post('/albums', albumValidationRules.createRules, albumController.store);
 router.put('/albums/:albumId', albumValidationRules.updateRules, albumController.update);
 
 module.exports = router;
+
+
+/* Lägg till ett album till den inloggade användaren */
+// POST /albums
+router.post('/albums', auth.validateJwtToken, profileValidationRules.addAlbumRules, profileController.addAlbum);
+
+/* Uppdatera ett album */
+// PUT /albums/:albumId
+router.put('/albums/:albumId', profileValidationRules.updateRules, profileController.updateAlbum);
