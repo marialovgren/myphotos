f51791a7b850e8cb0a8710f936e2b40367ed5c35
marialovgren <marialovgren86@hotmail.com>
2022-03-07 13:28:38 +0100

    added routes for photos

---
+++ routes/photo_route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const photoController = require('../controllers/photo_controller');
+const photoValidationRules = require('../validation/photo_validation');
+
+/* Hämta alla foton till den inloggade användaren */
+// GET /photos
+router.get('/', photoController.getPhotos);
+
+/* Hämta ett specifikt foto */
+// GET /photos/:photoId exempelvis /photos/1
+router.get('/:photosId', photoController.showPhotos);
+
+/* Lägg till ett foto */
+// POST /photos
+router.post('/', photoValidationRules.createRules, photoController.addPhoto);
+
+/* Uppdatera ett foto */
+// PUT /photos/:photoId exempelvis /photos/1
+router.put('/:photoId', photoValidationRules.updateRules, photoController.updatePhoto);
+
+module.exports = router;
